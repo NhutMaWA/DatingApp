34f1dcfea1df59a7d8f4ba2108f3635fc5401aea
nhut.ma <nhut.ma@WS41>
2020-05-04 16:39:30 +0700

    Finish section 6

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
 @Component({
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DatingApp-SPA';
+  jwtHelper = new JwtHelperService();
+
+  constructor (private authService: AuthService){}
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if(token){
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+  }
+}
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService,
+  constructor(public authService: AuthService,
               private alertify: AlertifyService) { }
 
   ngOnInit() {
